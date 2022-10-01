6738d5f72a3bbb546ea175a34196ef080622a0d1
Maxi Soriano <69224410+MaxiSoriano70@users.noreply.github.com>
2022-10-01 16:34:20 -0300

    Maquetado II

--- lib/src/components/bingo_card.dart
+++ lib/src/components/bingo_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class BingoCard extends StatefulWidget {
@@ -13,7 +14,49 @@ class _BingoCardState extends State<BingoCard> {
     return _bingoCard(/*200*/);
   }
 
-  Widget _bingoCard(/*double _width*/) {
+  Widget _bingoCard() {
+    List<String> listaDeNumeros=["1","2","3","4","5","6","7",
+      "9","10","11","12","13","14","15",
+      "1","2","3","4","5","6","7",
+      "1","2","3","4","5","6","7",
+      "1","2","3","4","5","6","7",];
+
+    List<String> listaConLetras(List<String> list){
+      List<String> listaConBingo=[];
+      int i=0;
+      int posicion=0;
+      while(i<40){
+        if(i==0){
+          listaConBingo.add("B");
+          i++;
+        }
+        else if(i==8){
+          listaConBingo.add("I");
+          i++;
+        }
+        else if(i==16){
+          listaConBingo.add("N");
+          i++;
+        }
+        else if(i==24){
+          listaConBingo.add("G");
+          i++;
+        }
+        else if(i==32){
+          listaConBingo.add("O");
+          i++;
+        }
+        else{
+          listaConBingo.add(list[posicion]);
+          i++;
+          posicion++;
+        }
+      }
+      return listaConBingo;
+    }
+    List<String> listaConBingo=[];
+    listaConBingo=listaConLetras(listaDeNumeros);
+
     return Material(
       elevation: 25,
       child: ClipRRect(
@@ -25,58 +68,30 @@ class _BingoCardState extends State<BingoCard> {
             borderRadius: BorderRadius.circular(25),
             color: Colors.grey,
           ),
-          child: Row(
-              children: [
-                Container(
-                  width: 72,
-                  height: MediaQuery.of(context).size.width*0.6364,
-                  child: _GridViewLetter(),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width-72,
-                  height: MediaQuery.of(context).size.width*0.6364,
-                  color: Colors.brown,
-                  child: _GridViewNumber(),)
-            ],
-          ),
+          child: GridView.builder(
+              itemCount: listaConBingo.length,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 8,
+              ),
+              itemBuilder: (context, index){
+                return _LetterOrNumber(context,index);
+            },
         ),
       ),
-    );
+    ));
   }
 
-  Widget _GridViewLetter() {
-    return GridView(
-      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 1,
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10
-      ),
-      children: [
-        _cardLetter("B", Colors.green, Colors.white),
-        _cardLetter("I", Colors.green, Colors.white),
-        _cardLetter("N", Colors.green, Colors.white),
-        _cardLetter("G", Colors.green, Colors.white),
-        _cardLetter("O", Colors.green, Colors.white),
-      ],
-    );
+  Widget _LetterOrNumber(context, index){
+    if(index==0 || index==8 || index==16 || index==24 || index==32){
+      return _cardLetter(context, index);
+    }
+    else {
+      return _buttonNumber(context, index);
+    }
   }
 
-  Widget _cardLetter(String letter, Color colorBackground, Color colorText) {
-    return Container(
-      width: 15,
-      height: 15,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: colorBackground,
-      ),
-      child: Center(
-        child: Text(letter, style: TextStyle(color: colorText, fontSize: 25),),
-      ),
-    );
-  }
-
-  Widget _ButtonNumber(String number,Color colorBackground, Color colorText){
+  Widget _cardLetter(context, index/*String letter, Color colorBackground, Color colorText*/) {
     return InkWell(
       onTap: (){},
       child: Container(
@@ -84,60 +99,36 @@ class _BingoCardState extends State<BingoCard> {
         height: 15,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
-          color: colorBackground,
+          color: Colors.green,
         ),
         child: Center(
-          child: Text(number, style: TextStyle(color: colorText, fontSize: 25),),
+          child: Text(context[index], style: const TextStyle(color: Colors.white, fontSize: 15),),
         ),
       ),
     );
   }
 
-  Widget _GridViewNumber(){
-    return GridView(
-      padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 7,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10
+  Widget _buttonNumber(context, index/*String number,Color colorBackground, Color colorText*/){
+    Color colorBacBackground=Colors.pink;
+    bool checkColor=true;
+    return InkWell(
+      onTap: (){
+        setState(() {
+          checkColor=!checkColor;
+          colorBacBackground=checkColor==true?Colors.pink:colorBacBackground=Colors.indigo;
+        });
+      },
+      child: Container(
+        width: 15,
+        height: 15,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: colorBacBackground,
+        ),
+        child: Center(
+          child: Text(context[index], style: const TextStyle(color: Colors.white, fontSize: 15),),
+        ),
       ),
-      children: [
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-        _ButtonNumber("1", Colors.green, Colors.white),
-        _ButtonNumber("2", Colors.green, Colors.white),
-        _ButtonNumber("3", Colors.green, Colors.white),
-        _ButtonNumber("4", Colors.green, Colors.white),
-        _ButtonNumber("5", Colors.green, Colors.white),
-      ],
     );
   }
 }
